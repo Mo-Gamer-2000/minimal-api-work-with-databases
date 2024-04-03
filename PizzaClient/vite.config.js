1dbed2acb5b990b672f0e4179bfbfd261ec6484d
Moeez Abdul <moeezrajput2000@gmail.com>
2024-04-03 11:42:49 +0000

    Exercise - Create an API - Unit 5 of 10

--- PizzaClient/vite.config.js
+++ PizzaClient/vite.config.js
@@ -6,5 +6,13 @@ export default defineConfig({
   plugins: [react()],
   server: {
     port: 3000,  // Client port
+    proxy: {
+      '/pizzas': {
+        target: 'http://localhost:5100', // Mock server port
+        changeOrigin: true,
+        secure: false,
+        ws: true,
+      }
+    }
   }
 })
